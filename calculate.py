8aacf25f1383a4df4b4045ea9245df54648030a8
mehdiaq37 <134053580+mehdiaq37@users.noreply.github.com>
2023-05-19 14:39:08 +0330

    بروزرسانی فایل

--- calculate.py
+++ calculate.py
@@ -14,6 +14,50 @@ def multiply(x, y):
 # This function divides two numbers
 def divide(x, y):
     return x / y
+# This function addition  two numbers
+def addition(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+def addition(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+# This function addition  two numbers
+def addition(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
 
 
 cal()
